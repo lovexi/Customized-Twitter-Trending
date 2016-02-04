8035a9f97dde57054a781f77d20251f22e0db13b
lovexi <scanletfever@gmail.com>
2016-02-04 12:14:23 -0600

    Modify Constants for customized parameters.

--- src/main/java/com/twittertrending/Constants.java
+++ src/main/java/com/twittertrending/Constants.java
@@ -11,11 +11,11 @@ public class Constants{
   public static final String COUNT = "count";
 
   //Customized part, you need to find all access from your developer account
-  public static final String ACCESSTOKENSECRET = "yRKqz7ol0a35oZgSGSMaOIPavwUnmkod2jzfmAWzQ1t6a";
-  public static final String ACCESSTOKEN = "708215277-yOP9n5hpMIF0QJo6FHaJMCT0Sli3Gtu41zpnZDMN";
-  public static final String CONSUMERSECRET = "p06130kG6pdY6qQ8Wk4ERSyvF5pM0cXtmk4CKhfCwhGU20aUIf";
-  public static final String CONSUMERKEY = "Wf7oZ3pl8gsotbGk3Pm0esWuJ";
+  public static final String ACCESSTOKENSECRET = "";
+  public static final String ACCESSTOKEN = "";
+  public static final String CONSUMERSECRET = "";
+  public static final String CONSUMERKEY = "";
 
   //These are the key words for trending topics.
-  public static final String[] KEYWORDS = {"china", "chinese", "Beijing"};
+  public static final String[] KEYWORDS = {"", "", "",...};
 }
